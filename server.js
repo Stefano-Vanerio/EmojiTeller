9d5e06f7dc1492283a98966589be08158a8c294c
Stefano-Vanerio <61703922+Stefano-Vanerio@users.noreply.github.com>
2021-10-19 10:08:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ io.on('connection', socket => {
     console.log(playerList.length);
 
     io.to("place").emit('roomUsers', {
-      length: playerList.length;
+      length: playerList.length,
     });
   });
 
